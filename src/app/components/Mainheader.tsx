7c7b3ce3cde47a44e2c36367a887f224d503f767
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2024-12-08 15:24:55 +0100

    Revert "Base skeleton"
    This reverts commit d8f5d73a5ff089134c5b705d4f828ba26f2641a2.

--- src/app/components/Mainheader.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Mainheader() {
-  return <div>Mainheader</div>;
-}
-
-export default Mainheader;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,17 @@
 import type { Metadata } from "next";
-import Mainheader from "./components/Mainheader";
-import Mainfooter from "./components/Mainfooter";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,10 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <Mainheader />
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         {children}
-        <Mainfooter />
       </body>
     </html>
   );
