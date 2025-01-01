699e22dec04e33dfc3c03bc4e8f5392584056759
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2025-01-01 17:27:34 +0100

    editing for bug

--- src/app/components/Earning.tsx
+++ src/app/components/Earning.tsx
@@ -1,59 +1,90 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { FaMoneyBillWave } from "react-icons/fa";
 import { BiMoneyWithdraw } from "react-icons/bi";
 import { Bar, Line, Pie } from "react-chartjs-2";
+import { useRouter } from "next/navigation";
 import axios from "axios";
 import "chart.js/auto";
+import { Toast } from "./Toast";
+
+// Interface for a single ticket type
+interface TicketType {
+  name: string; // e.g., "VIP", "NORMAL", "Basic"
+  sold: string; // Number of tickets sold as a string
+  price: string; // Price per ticket as a string
+  quantity: string; // Total available tickets as a string
+}
+
+// Interface for a single event
+interface Event {
+  id: string; // Unique identifier for the event
+  title: string; // Title of the event
+  slug: string; // URL-friendly string identifier
+  description: string; // Description of the event
+  image: string; // URL to the event image
+  date: string; // Event date in ISO format
+  location: string; // Event location
+  time: string; // Event time
+  venue: string; // Event venue
+  hostName: string; // Host of the event
+  ticketType: TicketType[]; // Array of ticket types
+  gallery: string | null; // Gallery links or null
+  socialMediaLinks: string | null; // Social media links or null
+  userId: string; // User ID of the organizer
+  createdAt: string; // Creation timestamp in ISO format
+  updatedAt: string; // Update timestamp in ISO format
+}
+
+// Interface for the overall API response
+interface EventsResponse {
+  events: Event[]; // Array of events
+}
 
 const Earnings = () => {
-  // Interface for a single ticket type
-  interface TicketType {
-    name: string; // e.g., "VIP", "NORMAL", "Basic"
-    sold: string; // Number of tickets sold as a string
-    price: string; // Price per ticket as a string
-    quantity: string; // Total available tickets as a string
-  }
-
-  // Interface for a single event
-  interface Event {
-    id: string; // Unique identifier for the event
-    title: string; // Title of the event
-    slug: string; // URL-friendly string identifier
-    description: string; // Description of the event
-    image: string; // URL to the event image
-    date: string; // Event date in ISO format
-    location: string; // Event location
-    time: string; // Event time
-    venue: string; // Event venue
-    hostName: string; // Host of the event
-    ticketType: TicketType[]; // Array of ticket types
-    gallery: string | null; // Gallery links or null
-    socialMediaLinks: string | null; // Social media links or null
-    userId: string; // User ID of the organizer
-    createdAt: string; // Creation timestamp in ISO format
-    updatedAt: string; // Update timestamp in ISO format
-  }
-
-  // Interface for the overall API response
-  interface EventsResponse {
-    events: Event[]; // Array of events
-  }
-
+  const router = useRouter();
   const [ticketSales, setTicketSales] = useState<
     EventsResponse | Event[] | null
   >([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [expandedRows, setExpandedRows] = useState<number[]>([]);
+  const [showToast, setShowToast] = useState<boolean>(false);
+  const [toastProps, setToastProps] = useState<{
+    type: "success" | "error" | "warning" | "info";
+    message: string;
+  }>({
+    type: "success",
+    message: "",
+  });
+
+  const toast = (
+    type: "success" | "error" | "warning" | "info",
+    message: string
+  ) => {
+    setToastProps({ type, message });
+    setShowToast(true);
+  };
+  const handleAxiosError = useCallback(
+    (error: unknown, defaultMessage: string) => {
+      let errorMessage = defaultMessage;
+      if (axios.isAxiosError(error)) {
+        errorMessage = error.response?.data?.message || error.message;
+      } else if (error instanceof Error) {
+        errorMessage = error.message;
+      }
+      toast("error", errorMessage);
+    },
+    []
+  );
 
   useEffect(() => {
-    let datar: Event[] = [];
-
     const fetchTicketSales = async () => {
+      setLoading(true);
       try {
         const token = localStorage.getItem("token");
         if (!token) {
-          throw new Error("User not authenticated");
+          toast("error", "Authentication token is missing. Please log in");
+          return;
         }
         const response = await axios.get(
           "https://v-ticket-backend.onrender.com/api/v1/events/my-events",
@@ -64,12 +95,22 @@ const Earnings = () => {
           }
         );
 
-        datar = response.data.events;
-        setTicketSales(response.data.events);
+        const allEarnings = response.data?.events;
+        if (response.status === 401) {
+          toast("error", "Unauthorized. Please log in again.");
+          router.push("/auth/login");
+        }
+        // setTicketSales(response.data.events);
         // console.log(ticketSales);
-      } catch (err) {
-        setError("Failed to fetch ticket sales. Please try again later.");
-        console.error("Error fetching ticket sales:", err);
+        if (Array.isArray(allEarnings)) {
+          setTicketSales(allEarnings);
+        } else {
+          throw new Error("Invalid response format");
+        }
+      } catch (error) {
+        console.error("Error fetching ticket sales:", error);
+        setError(error);
+        handleAxiosError(error, "Failed to fetch events");
       } finally {
         setLoading(false);
       }
@@ -77,12 +118,8 @@ const Earnings = () => {
       // console.log(datar);
     };
     //setTicketSales(response.data.events);
-    return () => {
-      fetchTicketSales();
-      setTicketSales(datar);
-      // Cleanup function
-    };
-  }, []);
+    fetchTicketSales();
+  }, [handleAxiosError, router]);
   // console.log("Ticket Sales:", ticketSales);
 
   // const ticketSales = [
@@ -109,7 +146,8 @@ const Earnings = () => {
     (total, sale) =>
       total +
       (sale.ticketType?.reduce(
-        (subtotal, type) => subtotal + parseFloat(type.price) * parseFloat(type.sold),
+        (subtotal, type) =>
+          subtotal + parseFloat(type.price) * parseFloat(type.sold),
         0
       ) || 0),
     0
@@ -236,6 +274,14 @@ const Earnings = () => {
 
   return (
     <section className="bg-gradient-to-br from-blue-50 to-white dark:from-gray-800 dark:to-gray-900 p-4 md:p-8 sm:p-2 rounded-xl shadow-lg max-w-5xl mx-auto space-y-6 md:space-y-8">
+      {showToast && (
+        <Toast
+          type={toastProps.type}
+          message={toastProps.message}
+          onClose={() => setShowToast(false)}
+        />
+      )}
+
       {/* Total Earnings Card */}
       <div className="p-4 md:p-6 bg-blue-100 dark:bg-blue-900 rounded-xl shadow-lg flex flex-col md:flex-row items-center justify-between space-y-4 md:space-y-0">
         <div className="flex items-center space-x-4">
@@ -281,84 +327,85 @@ const Earnings = () => {
             </tr>
           </thead>
           <tbody>
-            {eventsArray && eventsArray
-              .sort((a, b) => {
-                const totalA =
-                  a.ticketType?.reduce(
-                    (total, type) =>
-                      total + parseFloat(type.price) * parseFloat(type.sold),
-                    0
-                  ) || 0;
+            {eventsArray &&
+              eventsArray
+                .sort((a, b) => {
+                  const totalA =
+                    a.ticketType?.reduce(
+                      (total, type) =>
+                        total + parseFloat(type.price) * parseFloat(type.sold),
+                      0
+                    ) || 0;
 
-                const totalB =
-                  b.ticketType?.reduce(
-                    (total, type) =>
-                      total + parseFloat(type.price) * parseFloat(type.sold),
-                    0
-                  ) || 0;
+                  const totalB =
+                    b.ticketType?.reduce(
+                      (total, type) =>
+                        total + parseFloat(type.price) * parseFloat(type.sold),
+                      0
+                    ) || 0;
 
-                return totalB - totalA; // Sort from highest to lowest
-              })
-              .map((sale, index) => (
-                <React.Fragment key={index}>
-                  <tr className="odd:bg-white even:bg-gray-50 dark:odd:bg-gray-800 dark:even:bg-gray-700">
-                    <td className="p-3 md:p-4">{sale.title}</td>
-                    <td className="p-3 md:p-4">
-                      <button
-                        onClick={() => toggleExpandRow(index)}
-                        className="text-blue-600 dark:text-blue-400 underline"
-                      >
-                        {expandedRows.includes(index)
-                          ? "Hide Details"
-                          : "Show Details"}
-                      </button>
-                    </td>
-                    <td className="p-3 md:p-4 font-semibold">
-                      {formatCurrency(
-                        sale.ticketType?.reduce(
-                          (total, type) =>
-                            total +
-                            parseFloat(type.price) * parseFloat(type.sold),
-                          0
-                        )
-                      )}
-                    </td>
-                  </tr>
-                  {expandedRows.includes(index) && (
-                    <tr className="bg-gray-50 dark:bg-gray-700">
-                      <td colSpan={3} className="p-4">
-                        <table className="w-full text-gray-700 dark:text-gray-300">
-                          <thead>
-                            <tr>
-                              <th className="p-2 text-left">Type</th>
-                              <th className="p-2 text-left">Price</th>
-                              <th className="p-2 text-left">Sold</th>
-                              <th className="p-2 text-left">Revenue</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            {sale.ticketType?.map((type, i) => (
-                              <tr key={i}>
-                                <td className="p-2">{type.name}</td>
-                                <td className="p-2">
-                                  {formatCurrency(parseFloat(type.price))}
-                                </td>
-                                <td className="p-2">{type.sold}</td>
-                                <td className="p-2">
-                                  {formatCurrency(
-                                    parseFloat(type.price) *
-                                      parseFloat(type.sold)
-                                  )}
-                                </td>
-                              </tr>
-                            ))}
-                          </tbody>
-                        </table>
+                  return totalB - totalA; // Sort from highest to lowest
+                })
+                .map((sale, index) => (
+                  <React.Fragment key={index}>
+                    <tr className="odd:bg-white even:bg-gray-50 dark:odd:bg-gray-800 dark:even:bg-gray-700">
+                      <td className="p-3 md:p-4">{sale.title}</td>
+                      <td className="p-3 md:p-4">
+                        <button
+                          onClick={() => toggleExpandRow(index)}
+                          className="text-blue-600 dark:text-blue-400 underline"
+                        >
+                          {expandedRows.includes(index)
+                            ? "Hide Details"
+                            : "Show Details"}
+                        </button>
+                      </td>
+                      <td className="p-3 md:p-4 font-semibold">
+                        {formatCurrency(
+                          sale.ticketType?.reduce(
+                            (total, type) =>
+                              total +
+                              parseFloat(type.price) * parseFloat(type.sold),
+                            0
+                          )
+                        )}
                       </td>
                     </tr>
-                  )}
-                </React.Fragment>
-              ))}
+                    {expandedRows.includes(index) && (
+                      <tr className="bg-gray-50 dark:bg-gray-700">
+                        <td colSpan={3} className="p-4">
+                          <table className="w-full text-gray-700 dark:text-gray-300">
+                            <thead>
+                              <tr>
+                                <th className="p-2 text-left">Type</th>
+                                <th className="p-2 text-left">Price</th>
+                                <th className="p-2 text-left">Sold</th>
+                                <th className="p-2 text-left">Revenue</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              {sale.ticketType?.map((type, i) => (
+                                <tr key={i}>
+                                  <td className="p-2">{type.name}</td>
+                                  <td className="p-2">
+                                    {formatCurrency(parseFloat(type.price))}
+                                  </td>
+                                  <td className="p-2">{type.sold}</td>
+                                  <td className="p-2">
+                                    {formatCurrency(
+                                      parseFloat(type.price) *
+                                        parseFloat(type.sold)
+                                    )}
+                                  </td>
+                                </tr>
+                              ))}
+                            </tbody>
+                          </table>
+                        </td>
+                      </tr>
+                    )}
+                  </React.Fragment>
+                ))}
           </tbody>
         </table>
       </div>
