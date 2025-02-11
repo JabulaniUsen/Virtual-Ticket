3955d49345c1706043f921299fe7e0d84a0a9715
MathenaDev <mazinoishioma@gmail.com>
2025-02-11 09:53:32 +0100

    change base_url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 import Hero from "./components/home/Hero";
 import Trending from "./components/home/Trending";
-// import Mainheader from "./components/home/Mainheader";
 import Footer from "./components/home/Footer";
 import Header from "./components/home/Header";
 import FeaturedEvent from "./components/home/FeaturedEvent";
 import AllEvents from "./components/home/AllEvents";
-// import Pricing from "./components/home/PricingCard";
 import Tutorial from "./components/home/Tutorial";
 import LatestEvent from "./components/home/LatestEvent";
 import EventCalendar from '@/components/Calendar/EventCalendar';
 
 export default function Home() {
+
+
   return (
     <main>
       <EventCalendar />
@@ -20,7 +20,6 @@ export default function Home() {
       <LatestEvent />
       <AllEvents />
       <Trending />
-      {/* <Pricing /> */}
       <Tutorial />
       <Footer />
     </main>
