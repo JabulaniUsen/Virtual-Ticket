4faa61d5a39d99b1434ce9deeb725d35cfdb8f03
MathenaDev <mazinoishioma@gmail.com>
2025-03-14 04:19:12 +0100

    feature: added chatbot feature- pull latest update

--- src/components/Chatbot/section/ChatHeader.tsx
+++ src/components/Chatbot/section/ChatHeader.tsx
@@ -1,22 +1,33 @@
-import { FaRobot, FaTimes } from 'react-icons/fa';
+import { FaRobot, FaTimes, FaTrash } from 'react-icons/fa';
 
 interface ChatHeaderProps {
     onClose: () => void;
+    deleteHistory: () => void;
 }
 
-const ChatHeader = ({ onClose }: ChatHeaderProps) => {
+const ChatHeader = ({ onClose, deleteHistory }: ChatHeaderProps) => {
+
+
     return (
-        <div className="p-4 bg-blue-600 text-white flex justify-between items-center">
-            <div className="flex items-center gap-2">
+        <div className="p-4 bg-blue-600 text-white flex justify-between items-center  pt-4">
+            <div className="flex items-center gap-2 justify-center">
                 <FaRobot className="text-xl" />
                 <h3 className="font-semibold">Virtual Assistant</h3>
             </div>
-            <button
-                onClick={onClose}
-                className="p-1 hover:bg-blue-700 rounded-full transition-colors"
-            >
-                <FaTimes />
-            </button>
+            <div className="flex items-center gap-2 justify-center">
+                <button
+                    onClick={deleteHistory}
+                    className="p-1 hover:bg-blue-700 rounded-full transition-colors"
+                >
+                    <FaTrash />
+                </button>
+                <button
+                    onClick={onClose}
+                    className="p-1 hover:bg-blue-700 rounded-full transition-colors"
+                >
+                    <FaTimes />
+                </button>
+            </div>
         </div>
     );
 };
