e8ac034bb8a0c258c230a9568443d48ab56f1c46
MathenaDev <mazinoishioma@gmail.com>
2025-04-08 16:11:13 +0100

    Redirect when server is down(503 error)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useState, useEffect } from "react";
 import Hero from "./components/home/Hero";
 import Trending from "./components/home/Trending";
 import Footer from "./components/home/Footer";
@@ -8,9 +10,38 @@ import Tutorial from "./components/home/Tutorial";
 import LatestEvent from "./components/home/LatestEvent";
 import EventCalendar from '@/components/Calendar/EventCalendar';
 import ChatBot from "../components/Chatbot/chatbot";
+import ServerDown from "./503/page";
+import { BASE_URL } from "@/config";
+import axios from "axios";
 
 export default function Home() {
+  const [isServerDown, setIsServerDown] = useState(false);
 
+  useEffect(() => {
+    const checkServerStatus = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}api/v1/events/all-events`);
+        if (response.status === 503) {
+          setIsServerDown(true);
+        } else {
+          setIsServerDown(false);
+        }
+      } catch (error) {
+        setIsServerDown(true);
+        console.log(error);
+      }
+    };
+
+    checkServerStatus();
+    // Check server status every 30 seconds
+    const interval = setInterval(checkServerStatus, 30000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (isServerDown) {
+    return <ServerDown />;
+  }
 
   return (
     <main>
