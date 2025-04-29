a246a2a0ff15381841cd330e24c95015d65316db
Golden Tech <techbygolden@gmail.com>
2025-04-29 04:33:55 +0100

    resolve merge conflict

---
+++ config.ts
@@ -0,0 +1,6 @@
+
+// export const BASE_URL = 'https://v-ticket.onrender.com/'; // for production
+export const BASE_URL = 'https://v-ticket-backend.onrender.com/'; // for local development
+
+export const API_URL = 'https://vchatbot.onrender.com/api';// for production
+// export const API_URL = 'http://localhost:8080/api'; // for local development
