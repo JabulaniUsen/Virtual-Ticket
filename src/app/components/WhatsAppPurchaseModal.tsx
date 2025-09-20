e0f2990d49e170304448d6cb7af9474713c71313
Usenobong Etokakpan <jabulani@hashfire.xyz>
2025-09-20 03:22:43 +0100

    Adding service charge

--- src/app/components/WhatsAppPurchaseModal.tsx
+++ src/app/components/WhatsAppPurchaseModal.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useState } from 'react';
-import { FaWhatsapp, FaTimes, FaUser, FaEnvelope, FaShoppingCart } from 'react-icons/fa';
+import { FaWhatsapp, FaTimes } from 'react-icons/fa';
 import { type Ticket } from '@/types/event';
 
 interface WhatsAppPurchaseModalProps {
